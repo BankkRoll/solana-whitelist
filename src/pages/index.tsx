00411ef3b5d4dd4bccfbe4b147bd74b9037768ea
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-02-05 20:37:43 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,9 @@ import {
 import {
   Credenza,
   CredenzaBody,
+  CredenzaClose,
   CredenzaContent,
+  CredenzaFooter,
   CredenzaHeader,
   CredenzaTrigger,
 } from "@/components/ui/credenza";
@@ -30,6 +32,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { GetServerSideProps } from "next";
 import { Input } from "@/components/ui/input";
+import dynamic from "next/dynamic";
 import { parseCookies } from "nookies";
 import { supabase } from "@/utils/supabaseClient";
 import { toast } from "sonner";
@@ -38,6 +41,13 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { useWalletBalanceCheck } from "@/utils/useBalance";
 import whitelistData from "@/config/config";
 
+const WalletMultiButtonDynamic = dynamic(
+  () =>
+    import("@solana/wallet-adapter-react-ui").then(
+      (mod) => mod.WalletMultiButton
+    ),
+  { ssr: false }
+);
 type TimeLeft = {
   days?: number;
   hours?: number;
@@ -59,6 +69,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 export default function Home({ discordUsername }: HomeProps) {
+  const wallet = useWallet();
   const { select, wallets, publicKey, disconnect } = useWallet();
   const isRegistrationOpen = useRegistrationStatus(whitelistData);
   const [isChecking, setIsChecking] = useState(false);
@@ -472,42 +483,61 @@ export default function Home({ discordUsername }: HomeProps) {
                         </Button>
                       ) : (
                         <Credenza>
-                          <CredenzaTrigger asChild>
-                            <Button className="text-xs">Select Wallet</Button>
-                          </CredenzaTrigger>
-                          <CredenzaContent className="max-w-sm mx-auto">
+                          {!wallet.connected && (
+                            <CredenzaTrigger asChild>
+                              <Button className="text-xs">
+                                Connect Wallet
+                              </Button>
+                            </CredenzaTrigger>
+                          )}
+                          <CredenzaContent className="h-[50svh] max-sm:max-h-[60svh]">
                             <CredenzaHeader className="text-center justify-center items-center">
                               <h2 className="text-2xl font-bold mb-4 text-center">
                                 Connect a Wallet on <br /> Solana to Continue
                               </h2>
                             </CredenzaHeader>
-                            <CredenzaBody>
+
+                            <CredenzaBody className="overflow-y-auto">
                               {wallets.map((wallet) => (
-                                <div
+                                <CredenzaClose
                                   key={wallet.adapter.name}
-                                  className="flex items-center justify-between p-3 hover:bg-gray-800 rounded cursor-pointer"
-                                  onClick={() => {
-                                    select(wallet.adapter.name);
-                                  }}
+                                  className="flex items-center justify-between p-3 hover:bg-gray-800 rounded cursor-pointer w-full"
                                 >
-                                  <div className="flex items-center">
-                                    <img
-                                      className="w-6 h-6 mr-2"
-                                      src={wallet.adapter.icon}
-                                      alt={wallet.adapter.name}
-                                    />
-                                    <span className="text-white">
-                                      {wallet.adapter.name}
-                                    </span>
+                                  <div
+                                    className="flex justify-between items-center w-full"
+                                    onClick={() => {
+                                      select(wallet.adapter.name);
+                                    }}
+                                  >
+                                    <div className="flex items-center">
+                                      <img
+                                        className="w-6 h-6 mr-2"
+                                        src={wallet.adapter.icon}
+                                        alt={wallet.adapter.name}
+                                      />
+                                      <span className="text-white">
+                                        {wallet.adapter.name}
+                                      </span>
+                                    </div>
+                                    {wallet.readyState === "Installed" && (
+                                      <div className="flex justify-end">
+                                        <span className="text-sm text-gray-400">
+                                          Detected
+                                        </span>
+                                      </div>
+                                    )}
                                   </div>
-                                  {wallet.readyState === "Installed" && (
-                                    <span className="text-sm text-gray-400">
-                                      Detected
-                                    </span>
-                                  )}
-                                </div>
+                                </CredenzaClose>
                               ))}
                             </CredenzaBody>
+
+                            <CredenzaFooter className="text-center">
+                              <img
+                                src="https://solana.com/_next/static/media/solanaLogo.74d35f7a.svg"
+                                alt="Solana Logo"
+                                className="w-full h-3"
+                              />
+                            </CredenzaFooter>
                           </CredenzaContent>
                         </Credenza>
                       )}
